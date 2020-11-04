086bf51feba9f7f8d35a202f85d914f021a606ae
MichaelLee0 <michaelylee0@gmail.com>
2020-11-04 10:38:33 -0800

    Changed VAE structure. Went with conv encoders

--- network/vae.py
+++ network/vae.py
@@ -1,88 +1,99 @@
-vae base credit:https://danijar.com/building-variational-auto-encoders-in-tensorflow/
+# VAE Template Credits: https://github.com/FelixMohr/Deep-learning-with-Python/blob/master/VAE.ipynb 
 
+import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-
+%matplotlib inline
 tfd = tf.contrib.distributions
 
-def make_encoder(data, code_size):
-	# Pass data through dense layer encoder
-	# "code_size" is size of latent encoding
-	x = tf.layers.flatten(data)
-	x = tf.layers.dense(x, 200, tf.nn.relu)
-	x = tf.layers.dense(x, 200, tf.nn.relu)
-	# latent space
-	loc = tf.layers.dense(x, code_size)
-	scale = tf.layers.dense(x, code_size, tf.nn.softplus)
-	# Generates multivariate gaussian distributions of size n = code_size(or latent space dim), 
-	# scaling "squishes" the distribution to enhance certain latent variables based on their values
-	return tfd.MultivariateNormalDiag(loc, scale)
+## Image preprocessing
 
-def make_prior(code_size):
-	# Creates the prior distribution used to calculate KL divergence (nomal multivar. dist w/ unit var.)
-	loc = tf.zeros(code_size)
-	scale = tf.ones(code_size)
-	return tfd.MultivariateNormalDiag(loc, scale)
+#Resets current thread graph
+tf.reset_default_graph()
 
+batch_size = 64
 
-def make_decoder(code, data_shape):
-	x = code
-	x = tf.layers.dense(x, 200, tf.nn.relu)
-	x = tf.layers.dense(x, 200, tf.nn.relu)
-	logit = tf.layers.dense(x, np.prod(data_shape))
-	logit = tf.reshape(logit, [-1] + data_shape)
-	return tfd.Independent(tfd.Bernoulli(logit), 2)
+## Define placeholder inputs to net
+X_in = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='X')
+Y    = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='Y')
+	# Used to compute losses
+Y_flat = tf.reshape(Y, shape=[-1, 28 * 28])
+	# Determine application of dropout 
+keep_prob = tf.placeholder(dtype=tf.float32, shape=(), name='keep_prob')
+
+# How many color channels do we want and size of latent space
+dec_in_channels = 1
+n_latent = 8
+
+reshaped_dim = [-1, 7, 7, dec_in_channels]
+inputs_decoder = 49 * dec_in_channels / 2
+
+## Define leaky relu loss function
+def lrelu(x, alpha=0.3):
+    return tf.maximum(x, tf.multiply(x, alpha))
 
 
-def plot_codes(ax, codes, labels):
-	ax.scatter(codes[:, 0], codes[:, 1], s=2, c=labels, alpha=0.1)
-	ax.set_aspect('equal')
-	ax.set_xlim(codes.min() - .1, codes.max() + .1)
-	ax.set_ylim(codes.min() - .1, codes.max() + .1)
-	ax.tick_params(
-		axis='both', which='both', left='off', bottom='off',
-		labelleft='off', labelbottom='off')
+def encoder(X_in, keep_prob):
+    activation = lrelu
+	# Defines the scope of the conv'd data within "ecoder" 
+    with tf.variable_scope("encoder", reuse=None):
+        X = tf.reshape(X_in, shape=[-1, 28, 28, 1])
+        x = tf.layers.conv2d(X, filters=64, kernel_size=4, strides=2, padding='same', activation=activation)
+        x = tf.nn.dropout(x, keep_prob)			# Add maxpool layers?
+        x = tf.layers.conv2d(x, filters=64, kernel_size=4, strides=2, padding='same', activation=activation)
+        x = tf.nn.dropout(x, keep_prob)
+        x = tf.layers.conv2d(x, filters=64, kernel_size=4, strides=1, padding='same', activation=activation)
+        x = tf.nn.dropout(x, keep_prob)
+        x = tf.contrib.layers.flatten(x)
+		# Define mean, std dev and epsilon & create z: normal dist. centered around the means
+        mn = tf.layers.dense(x, units=n_latent)
+        sd       = 0.5 * tf.layers.dense(x, units=n_latent)            
+        epsilon = tf.random_normal(tf.stack([tf.shape(x)[0], n_latent])) 
+        z  = mn + tf.multiply(epsilon, tf.exp(sd))
+        
+        return z, mn, sd
 
+def decoder(sampled_z, keep_prob):
+	# Edit Decoder for layers of dense layers with final output layer of size(5): dist & rel positions: pitch, yaw, roll 
+    with tf.variable_scope("decoder", reuse=None):
+        x = tf.layers.dense(sampled_z, units=inputs_decoder, activation=lrelu)
+        x = tf.layers.dense(x, units=inputs_decoder * 2 + 1, activation=lrelu)
+        x = tf.reshape(x, reshaped_dim)
+        x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=2, padding='same', activation=tf.nn.relu)
+        x = tf.nn.dropout(x, keep_prob)
+        x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=1, padding='same', activation=tf.nn.relu)
+        x = tf.nn.dropout(x, keep_prob)
+        x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=1, padding='same', activation=tf.nn.relu)
+        
+        x = tf.contrib.layers.flatten(x)
+        x = tf.layers.dense(x, units=28*28, activation=tf.nn.sigmoid)
+        img = tf.reshape(x, shape=[-1, 28, 28])
+        return img
 
-def plot_samples(ax, samples):
-	for index, sample in enumerate(samples):
-		ax[index].imshow(sample, cmap='gray')
-		ax[index].axis('off')
+## Connect encoder and decoder
+sampled, mn, sd = encoder(X_in, keep_prob)
+dec = decoder(sampled, keep_prob)
 
+## Compute loss to enforce guassian
+unreshaped = tf.reshape(dec, [-1, 28*28])
+img_loss = tf.reduce_sum(tf.squared_difference(unreshaped, Y_flat), 1)		# Edit this. Currently it takes difference between flattened OG input and generated image. We change to predicted dis.
+latent_loss = -0.5 * tf.reduce_sum(1.0 + 2.0 * sd - tf.square(mn) - tf.exp(2.0 * sd), 1)
+loss = tf.reduce_mean(img_loss + latent_loss)
+optimizer = tf.train.AdamOptimizer(0.0005).minimize(loss)
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
-data = tf.placeholder(tf.float32, [None, 28, 28])
+## Train
+for i in range(30000):
+    batch = [np.reshape(b, [28, 28]) for b in mnist.train.next_batch(batch_size=batch_size)[0]]
+    sess.run(optimizer, feed_dict = {X_in: batch, Y: batch, keep_prob: 0.8})
+        
+    if not i % 200:
+        ls, d, i_ls, d_ls, mu, sigm = sess.run([loss, dec, img_loss, latent_loss, mn, sd], feed_dict = {X_in: batch, Y: batch, keep_prob: 1.0})
+        plt.imshow(np.reshape(batch[0], [28, 28]), cmap='gray')
+        plt.show()
+        plt.imshow(d[0], cmap='gray')
+        plt.show()
+        print(i, ls, np.mean(i_ls), np.mean(d_ls))
 
-make_encoder = tf.make_template('encoder', make_encoder)
-make_decoder = tf.make_template('decoder', make_decoder)
-
-# Define the model.
-prior = make_prior(code_size=2)
-posterior = make_encoder(data, code_size=2)
-	# Samples a set of points from within the latent space distribution
-code = posterior.sample()
-
-# Define the loss using ELBO & KL divergence
-likelihood = make_decoder(code, [28, 28]).log_prob(data)
-divergence = tfd.kl_divergence(posterior, prior)
-elbo = tf.reduce_mean(likelihood - divergence)
-optimize = tf.train.AdamOptimizer(0.001).minimize(-elbo)
-
-samples = make_decoder(prior.sample(10), [28, 28]).mean()
-
-mnist = input_data.read_data_sets('MNIST_data/')
-fig, ax = plt.subplots(nrows=20, ncols=11, figsize=(10, 20))
-with tf.train.MonitoredSession() as sess:
-  for epoch in range(20):
-	feed = {data: mnist.test.images.reshape([-1, 28, 28])}
-	test_elbo, test_codes, test_samples = sess.run([elbo, code, samples], feed)
-	print('Epoch', epoch, 'elbo', test_elbo)
-	ax[epoch, 0].set_ylabel('Epoch {}'.format(epoch))
-	plot_codes(ax[epoch, 0], test_codes, mnist.test.labels)
-	plot_samples(ax[epoch, 1:], test_samples)
-	for _ in range(600):
-	  feed = {data: mnist.train.next_batch(100)[0].reshape([-1, 28, 28])}
-	  sess.run(optimize, feed)
-
-plt.savefig('vae-mnist.png', dpi=300, transparent=True, bbox_inches='tight')
+## Save weights
