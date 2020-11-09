234e62c69af3dddd26df3e069fb2c82aba90181a
MichaelLee0 <michaelylee0@gmail.com>
2020-11-08 20:50:28 -0800

    TensorFlow base version. Moving to Keras.

--- network/vae.py
+++ network/vae.py
@@ -1,99 +1,134 @@
-# VAE Template Credits: https://github.com/FelixMohr/Deep-learning-with-Python/blob/master/VAE.ipynb 
-
+# Import MNIST data
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
-tfd = tf.contrib.distributions
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-## Image preprocessing
-
-#Resets current thread graph
-tf.reset_default_graph()
-
-batch_size = 64
-
-## Define placeholder inputs to net
-X_in = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='X')
-Y    = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28], name='Y')
-	# Used to compute losses
-Y_flat = tf.reshape(Y, shape=[-1, 28 * 28])
-	# Determine application of dropout 
-keep_prob = tf.placeholder(dtype=tf.float32, shape=(), name='keep_prob')
-
-# How many color channels do we want and size of latent space
-dec_in_channels = 1
-n_latent = 8
-
-reshaped_dim = [-1, 7, 7, dec_in_channels]
-inputs_decoder = 49 * dec_in_channels / 2
-
-## Define leaky relu loss function
-def lrelu(x, alpha=0.3):
-    return tf.maximum(x, tf.multiply(x, alpha))
+## Image preprocessing - Adding noise, reshaping, padding (maybe? prob not) etc.
+# Y = rel. position, pitch, yaw, roll
 
 
-def encoder(X_in, keep_prob):
-    activation = lrelu
-	# Defines the scope of the conv'd data within "ecoder" 
-    with tf.variable_scope("encoder", reuse=None):
-        X = tf.reshape(X_in, shape=[-1, 28, 28, 1])
-        x = tf.layers.conv2d(X, filters=64, kernel_size=4, strides=2, padding='same', activation=activation)
-        x = tf.nn.dropout(x, keep_prob)			# Add maxpool layers?
-        x = tf.layers.conv2d(x, filters=64, kernel_size=4, strides=2, padding='same', activation=activation)
-        x = tf.nn.dropout(x, keep_prob)
-        x = tf.layers.conv2d(x, filters=64, kernel_size=4, strides=1, padding='same', activation=activation)
-        x = tf.nn.dropout(x, keep_prob)
-        x = tf.contrib.layers.flatten(x)
-		# Define mean, std dev and epsilon & create z: normal dist. centered around the means
-        mn = tf.layers.dense(x, units=n_latent)
-        sd       = 0.5 * tf.layers.dense(x, units=n_latent)            
-        epsilon = tf.random_normal(tf.stack([tf.shape(x)[0], n_latent])) 
-        z  = mn + tf.multiply(epsilon, tf.exp(sd))
-        
-        return z, mn, sd
+class package_predict():
+    def __init__(store_vals):
+        # Image Parameters
+        self.store_vals = store_vals
+        img_height = 240
+        img_width = 320
 
-def decoder(sampled_z, keep_prob):
-	# Edit Decoder for layers of dense layers with final output layer of size(5): dist & rel positions: pitch, yaw, roll 
-    with tf.variable_scope("decoder", reuse=None):
-        x = tf.layers.dense(sampled_z, units=inputs_decoder, activation=lrelu)
-        x = tf.layers.dense(x, units=inputs_decoder * 2 + 1, activation=lrelu)
-        x = tf.reshape(x, reshaped_dim)
-        x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=2, padding='same', activation=tf.nn.relu)
-        x = tf.nn.dropout(x, keep_prob)
-        x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=1, padding='same', activation=tf.nn.relu)
-        x = tf.nn.dropout(x, keep_prob)
-        x = tf.layers.conv2d_transpose(x, filters=64, kernel_size=4, strides=1, padding='same', activation=tf.nn.relu)
-        
-        x = tf.contrib.layers.flatten(x)
-        x = tf.layers.dense(x, units=28*28, activation=tf.nn.sigmoid)
-        img = tf.reshape(x, shape=[-1, 28, 28])
-        return img
+        # Training Parameters
+        learning_rate = 0.001
+        num_steps = 200
+        batch_size = 128
+        display_step = 10
 
-## Connect encoder and decoder
-sampled, mn, sd = encoder(X_in, keep_prob)
-dec = decoder(sampled, keep_prob)
+        # Network Parameters
+        num_input = img_height*img_width # MNIST data input (img shape: 28*28)
+        num_classes = 4 # Output rel. distance, pose (3)
+        dropout = 0.75 # Dropout, probability to keep units
 
-## Compute loss to enforce guassian
-unreshaped = tf.reshape(dec, [-1, 28*28])
-img_loss = tf.reduce_sum(tf.squared_difference(unreshaped, Y_flat), 1)		# Edit this. Currently it takes difference between flattened OG input and generated image. We change to predicted dis.
-latent_loss = -0.5 * tf.reduce_sum(1.0 + 2.0 * sd - tf.square(mn) - tf.exp(2.0 * sd), 1)
-loss = tf.reduce_mean(img_loss + latent_loss)
-optimizer = tf.train.AdamOptimizer(0.0005).minimize(loss)
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+        # tf Graph input
+        X = tf.placeholder(tf.float32, [None, num_input])
+        Y = tf.placeholder(tf.float32, [None, num_classes])
+        keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
 
-## Train
-for i in range(30000):
-    batch = [np.reshape(b, [28, 28]) for b in mnist.train.next_batch(batch_size=batch_size)[0]]
-    sess.run(optimizer, feed_dict = {X_in: batch, Y: batch, keep_prob: 0.8})
-        
-    if not i % 200:
-        ls, d, i_ls, d_ls, mu, sigm = sess.run([loss, dec, img_loss, latent_loss, mn, sd], feed_dict = {X_in: batch, Y: batch, keep_prob: 1.0})
-        plt.imshow(np.reshape(batch[0], [28, 28]), cmap='gray')
-        plt.show()
-        plt.imshow(d[0], cmap='gray')
-        plt.show()
-        print(i, ls, np.mean(i_ls), np.mean(d_ls))
 
-## Save weights
+    # Create some wrappers for simplicity
+    @staticmethod
+    def conv2d(x, W, b, strides=1):
+        # Conv2D wrapper, with bias and relu activation
+        x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+        x = tf.nn.bias_add(x, b)
+        return tf.nn.relu(x)
+
+    @staticmethod
+    def maxpool2d(x, k=2):
+        # MaxPool2D wrapper
+        return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+                            padding='SAME')
+
+
+    # Create model
+    @staticmethod
+    def conv_net(x, weights, biases, dropout):
+        # Initialize layers weight & bias
+        weights = {
+            # 5x5 conv, 1 input, 32 outputs
+            'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+            # 5x5 conv, 32 inputs, 64 outputs
+            'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+            # fully connected, 7*7*64 inputs, 1024 outputs
+            'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
+            # 1024 inputs, 4 outputs (class prediction)
+            'out': tf.Variable(tf.random_normal([1024, num_classes]))
+        }  
+
+        biases = {
+            'bc1': tf.Variable(tf.random_normal([32])),
+            'bc2': tf.Variable(tf.random_normal([64])),
+            'bd1': tf.Variable(tf.random_normal([1024])),
+            'out': tf.Variable(tf.random_normal([num_classes]))
+        }
+
+        # Reshape to match picture format [Height x Width x Channel]
+        # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
+        x = tf.reshape(x, shape=[-1, img_height, img_width, 1])
+
+        # Convolution Layer
+        conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+        # Max Pooling (down-sampling)
+        conv1 = maxpool2d(conv1, k=2)
+
+        # Convolution Layer
+        conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+        # Max Pooling (down-sampling)
+        conv2 = maxpool2d(conv2, k=2)
+
+        # Fully connected layer
+        # Reshape conv2 output to fit fully connected layer input
+        fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+        fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+        fc1 = tf.nn.relu(fc1)
+        # Apply Dropout
+        fc1 = tf.nn.dropout(fc1, dropout)
+
+        # Output, class prediction
+        out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+        return out
+
+    @staticmethod
+    def train(self.store_vals):
+
+        # Construct model
+        prediction = conv_net(X, weights, biases, keep_prob)
+
+        # Define loss and optimizer
+        loss_op = tf.reduce_mean(tf.squared_difference(prediction, Y))
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        train_op = optimizer.minimize(loss_op)
+
+        # Training Loop
+        hm_epochs = 10
+        with tf.Session() as sess:
+            sess.run(tf.initialize_all_variables())
+
+            for epoch in range(hm_epochs):
+                epoch_loss = 0
+                for _ in range(int(mnist.train.num_examples/batch_size)):
+                    epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+                    _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                    epoch_loss += c
+
+                print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
+
+            correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+            accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+            print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+        if store_vals:
+            variable = [weight for weights in tf.trainable_variables() if weight.name == name_my_var]
+
+        # Evaluate model
+        correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    def predict(x):
