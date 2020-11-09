94382045a9b4424d662beb070124ce2e8ad4258d
MichaelLee0 <michaelylee0@gmail.com>
2020-11-08 21:33:54 -0800

    Keras version. Need info on ecoding of state vars

--- network/vae.py
+++ network/vae.py
@@ -1,134 +1,105 @@
-# Import MNIST data
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
-## Image preprocessing - Adding noise, reshaping, padding (maybe? prob not) etc.
-# Y = rel. position, pitch, yaw, roll
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
 
 class package_predict():
-    def __init__(store_vals):
+    def __init__(image, store_vals):
+
         # Image Parameters
         self.store_vals = store_vals
-        img_height = 240
-        img_width = 320
+        self.img_height = 240
+        self.img_width = 320
 
-        # Training Parameters
-        learning_rate = 0.001
-        num_steps = 200
-        batch_size = 128
-        display_step = 10
+        self.batch_size = 128
 
         # Network Parameters
-        num_input = img_height*img_width # MNIST data input (img shape: 28*28)
-        num_classes = 4 # Output rel. distance, pose (3)
-        dropout = 0.75 # Dropout, probability to keep units
+        # num_input = img_height*img_width # MNIST data input (img shape: 28*28)
+        self.num_classes = 4 # Output rel. distance, pose (3)
 
         # tf Graph input
-        X = tf.placeholder(tf.float32, [None, num_input])
-        Y = tf.placeholder(tf.float32, [None, num_classes])
-        keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
+        # X = tf.placeholder(tf.float32, [None, num_input])
+        # Y = tf.placeholder(tf.float32, [None, num_classes])
+        # keep_prob = tf.placeholder(tf.float32) # dropout (keep probability)
 
+    def load_data_set(self):
+        ## Image preprocessing - Adding noise, reshaping, padding (maybe? prob not) etc.
+        # Y = rel. position, pitch, yaw, roll
+        batch_size = 32
+        data_dir = ""       # Set directory
+        train_ds = tf = tf.keras.preprocessing.image_dataset_from_directory(
+            data_dir, 
+            validation_split = 0.2, 
+            subset= "training",
+            seed=123,
+            image_size = (self.img_height, self.img_width),
+            batch_size = batch_size)
+        val_ds = tf = tf.keras.preprocessing.image_dataset_from_directory(
+            data_dir, 
+            validation_split = 0.2, 
+            subset= "training",
+            seed=123,
+            image_size = (self.img_height, self.img_width),
+            batch_size = self.batch_size)
 
-    # Create some wrappers for simplicity
+        ## Need to Know form of encoded labels for train_labels and test_labels
+        return train_ds, val_ds, train_labels, test_labels
+            
+    def def_train_model(self, save_model = True):
+
+        # Load dataset
+        train_ds, val_ds, train_labels, test_labels = load_data_set()
+
+        model = tf.keras.Sequential([
+            layers.experimental.preprocessing.Rescaling(1./255),
+            layers.Conv2D(32, 3, activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(64,activation = 'relu'),
+
+            layers.Dense(self.num_classes)
+        ])
+
+        model.compile(
+            optimizer='adam',
+            loss=tf.losses.MeanSquaredError())
+
+        history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels)
+
+        ## Validate performance
+        plt.plot(history.history['accuracy'], label='accuracy')
+        plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.ylim([0.5, 1])
+        plt.legend(loc='lower right')
+
+        test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+        # Save model 
+        if save_model:
+            model.save('/PackageRelModel')      # Alternatively can save as an H5 for computational savings. Test with wholesale first
+
     @staticmethod
-    def conv2d(x, W, b, strides=1):
-        # Conv2D wrapper, with bias and relu activation
-        x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
-        x = tf.nn.bias_add(x, b)
-        return tf.nn.relu(x)
+    def predict(img):
+        """
+        Args:
+        img = input png image from drone
 
-    @staticmethod
-    def maxpool2d(x, k=2):
-        # MaxPool2D wrapper
-        return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
-                            padding='SAME')
+        Output:
+        rel_pos = relative position
+        pitch = relative pitch
+        yaw = relative yaw
+        roll = relative roll
+        """
+        model =keras.models.load_model('/PackageRelModel')
+        rel_pos, pitch, yaw, roll = model.predict(img)
 
 
-    # Create model
-    @staticmethod
-    def conv_net(x, weights, biases, dropout):
-        # Initialize layers weight & bias
-        weights = {
-            # 5x5 conv, 1 input, 32 outputs
-            'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-            # 5x5 conv, 32 inputs, 64 outputs
-            'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-            # fully connected, 7*7*64 inputs, 1024 outputs
-            'wd1': tf.Variable(tf.random_normal([7*7*64, 1024])),
-            # 1024 inputs, 4 outputs (class prediction)
-            'out': tf.Variable(tf.random_normal([1024, num_classes]))
-        }  
-
-        biases = {
-            'bc1': tf.Variable(tf.random_normal([32])),
-            'bc2': tf.Variable(tf.random_normal([64])),
-            'bd1': tf.Variable(tf.random_normal([1024])),
-            'out': tf.Variable(tf.random_normal([num_classes]))
-        }
-
-        # Reshape to match picture format [Height x Width x Channel]
-        # Tensor input become 4-D: [Batch Size, Height, Width, Channel]
-        x = tf.reshape(x, shape=[-1, img_height, img_width, 1])
-
-        # Convolution Layer
-        conv1 = conv2d(x, weights['wc1'], biases['bc1'])
-        # Max Pooling (down-sampling)
-        conv1 = maxpool2d(conv1, k=2)
-
-        # Convolution Layer
-        conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
-        # Max Pooling (down-sampling)
-        conv2 = maxpool2d(conv2, k=2)
-
-        # Fully connected layer
-        # Reshape conv2 output to fit fully connected layer input
-        fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
-        fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
-        fc1 = tf.nn.relu(fc1)
-        # Apply Dropout
-        fc1 = tf.nn.dropout(fc1, dropout)
-
-        # Output, class prediction
-        out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
-        return out
-
-    @staticmethod
-    def train(self.store_vals):
-
-        # Construct model
-        prediction = conv_net(X, weights, biases, keep_prob)
-
-        # Define loss and optimizer
-        loss_op = tf.reduce_mean(tf.squared_difference(prediction, Y))
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        train_op = optimizer.minimize(loss_op)
-
-        # Training Loop
-        hm_epochs = 10
-        with tf.Session() as sess:
-            sess.run(tf.initialize_all_variables())
-
-            for epoch in range(hm_epochs):
-                epoch_loss = 0
-                for _ in range(int(mnist.train.num_examples/batch_size)):
-                    epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-                    _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
-                    epoch_loss += c
-
-                print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
-
-            correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-
-            accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-            print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
-
-        if store_vals:
-            variable = [weight for weights in tf.trainable_variables() if weight.name == name_my_var]
-
-        # Evaluate model
-        correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-    def predict(x):
